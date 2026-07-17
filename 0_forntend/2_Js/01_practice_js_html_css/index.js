3303a4ebdf8cb0372fba8d050219149e042d47c3
vivekmpatne <vivekpatnem@gmail.com>
2026-07-17 21:45:38 +0530

    added some practice set 1 ( html, css, js basic practice)

---
+++ 0_forntend/2_Js/01_practice_js_html_css/index.js
@@ -0,0 +1,12 @@
+console.log("Hello, World!");
+//alert("Hello, World!");
+console.error("This is an error message.");
+console.warn("This is a warning message.");
+console.info("This is an informational message.");
+console.debug("This is a debug message.");
+console.log("This is a log message.");
+console.log("This is a log message with multiple arguments:", "arg1", "arg2", "arg3");
+
+
+
+
